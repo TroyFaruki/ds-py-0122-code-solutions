9158e3bcf024891b6c85e3cf3f9e79cf9de9d161
Troy Faruki <farukitroy@gmail.com>
2022-01-03 20:47:59 -0800

    Completed ds prep example exercise

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+  # write what you would like to analyze in the quotes here 👇
+
+  "Oceans of data.",
+
+  # write what you would like to learn from that analysis here 👇
+
+  "to predict risk for insurance companies."
+
+))
