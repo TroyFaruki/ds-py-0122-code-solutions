6464588642300ed3debce8a0dfe35851218ce458
Troy Faruki <farukitroy@gmail.com>
2022-01-12 20:36:30 -0800

    Completed changes to ds-prep-python-lists

--- ds-prep-python-lists/main.py
+++ ds-prep-python-lists/main.py
@@ -1,8 +1,8 @@
 ages = [29, 19, 17, 26, 32, 16, 21]
 print(ages)
-print(ages[4:5])
-print(ages[-2:-1])
-print(ages[3:6])
+print(ages[4])
+print(ages[-2])
+print(ages[4:6])
 print(ages[:4])
 
 if 17 in ages:
@@ -15,7 +15,7 @@ ages[2] = 18
 print(ages)
 
 temp = ages[4]
-ages[3] = ages[4]
+ages[4] = ages[3]
 temp = ages[3]
 print("Instruction 15: ",ages)
 
